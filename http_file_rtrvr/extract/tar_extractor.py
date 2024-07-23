75b2b0d7ee7a0c798b83a15176b4619ee7ded1e9
Tim Perry <perry2of5@yahoo.com>
2024-07-23 15:36:49 -0700

    Add support for extracting tar files

--- http_file_rtrvr/extract/tar_extractor.py
+++ http_file_rtrvr/extract/tar_extractor.py
@@ -1,23 +1,26 @@
 from http_file_rtrvr.extract.abstract_extractor import AbstractExtractor
 from http_file_rtrvr.extract.extraction_exception import ExtractionException
-from zipfile import ZipFile 
+import tarfile
 import os
 
-class ZipExtractor(AbstractExtractor):
+class TarExtractor(AbstractExtractor):
+    tar_gz_long_form = ".tar.gz"
+
     def __init__(self):
         super(AbstractExtractor, self).__init__()
 
     def extract_to_temp_dir(
+            self,
             archive: str, 
             temp_dir: str) -> str:
         """
-        Extracts the contents of an zip to a temporary directory.
+        Extracts the contents of an .tar, .tar.gz, or .tgz to a temporary directory.
 
         Args:
-            archive: file to extract. Contents are extracted from <file>.zip to <temp_dir>/<file>/*
+            archive: file to extract. Contents are extracted from <file>.tgz to <temp_dir>/<file>/*
             temp_dir: path to temporary directory to extract archive contents to. The directory "temp_dir"
                 should exist and the contents will be extracted into a directory named by the archive file name 
-                but with the extension removed. I.e., files from "archive.zip" will be extracted to 
+                but with the extension removed. I.e., files from "archive.tgz" will be extracted to 
                 "temp_dir/archive".
 
         returns:
@@ -31,14 +34,23 @@ class ZipExtractor(AbstractExtractor):
         if not os.path.exists(archive):
             raise ExtractionException(f"Archive {archive} does not exist.")
         
+        if not tarfile.is_tarfile(archive):
+            raise ExtractionException(f"File {archive} is not a tar file (.tar, .tar.gz, or .tgz).")
+
         try:
             archive_name_with_ext = os.path.basename(archive)
             archive_name = os.path.splitext(archive_name_with_ext)[0]
+            if archive_name_with_ext.endswith(self.tar_gz_long_form):
+                # splitext doesn't know about .tar.gz as a single extension, so handle that case
+                archive_name = archive_name_with_ext[:(-1 * self.tar_gz_long_form.length)]
             extract_dir = os.path.join(temp_dir, archive_name)
             if not os.path.exists(extract_dir):
                 os.makedirs(extract_dir)
-            with ZipFile(archive, 'r') as zip_ref:
-                zip_ref.extractall(path=extract_dir)
+            print("Extracting to:", extract_dir)
+            with tarfile.open(archive, 'r') as archive_ref:
+                # filter='data' protects against directory traversal attacks
+                archive_ref.extractall(path=extract_dir, filter='data')
             return extract_dir
         except Exception as e:
             raise ExtractionException(f"Failed to extract {archive} to {extract_dir}.", e)
+            
