f3af578aa88ca97fd7598251eb16767658582c7c
perry2of5 <perry2of5@yahoo.com>
2024-07-23 13:47:26 -0700

    Add upload tests (#1)
    Add tests for uploads and fix defect discovered while doing so.
    
    * Add tests for and fix single file upload
    * Add integration test to upload a directory
    * Simplify error handling during blob upload
    
    Note: all tests assume if no exception is thrown the test is successful.
    At some point, we need to add logic to check to see the file was actually
    uploaded to an Azure blob.

--- http_file_rtrvr/uploader/abstract_file_tree_uploader.py
+++ http_file_rtrvr/uploader/abstract_file_tree_uploader.py
@@ -1,4 +1,8 @@
 import os
+from http_file_rtrvr.retrieval_request import RetrievalRequest
+
+from datetime import datetime
+
 
 class AbstractFileTreeUploader():
     """
@@ -22,17 +26,23 @@ class AbstractFileTreeUploader():
     def __init__(self):
         pass
 
-    def upload_file_tree(self, file_tree_path: str):
-            """
-            Uploads the contents of a file tree (directory, zip, etc.) to a blob storage.
+    def upload_file_tree(
+            self, 
+            file_tree_path: str,
+            rtrvl_req: RetrievalRequest,
+            download_time: datetime) -> None:
+        """
+        Uploads the contents of a file tree (directory, zip, etc.) to a blob storage.
 
-            Args:
-                directory_path (str): The path of the base directory to be uploaded. Uploads walk 
-                the tree of files and upload all files found replicating the folder structure
-                remotely.
+        Args:
+            directory_path (str): The path of the base directory to be uploaded. Uploads walk 
+                    the tree of files and upload all files found replicating the folder structure
+                    remotely.
+            rtrvl_req (RetrievalRequest): The retrieval request object containing the requested URL
+                    and the save_to prefix.
+            download_time (datetime): The timestamp of the download.
 
-            Returns:
-                None
-            """
-            pass
-
+        Returns:
+            None
+        """
+        pass
