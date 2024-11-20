f14d1ad59d7e1eac371540a33277fd2d8c69ef49
Tim Perry <perry2of5@yahoo.com>
2024-11-20 09:20:59 -0800

    install dependencies with pip since vscode isn't finding them from the poetry location

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -13,14 +13,10 @@ WORKSPACE_DIR=$(pwd)
 
 # Change some Poetry settings to better deal with working in a container
 poetry config cache-dir ${WORKSPACE_DIR}/.cache
-poetry config virtualenvs.in-project true
 
 # Now install all dependencies
-# poetry export --without-hashes --format=requirements.txt > requirements.txt
-# pip install -r poetry export --without-hashes --format=requirements.txt
-# rm requirements.txt
-
-poetry config virtualenvs.in-project true --local
-poetry install
+poetry export --without-hashes --format=requirements.txt > ./requirements.txt
+pip install -r ./requirements.txt
+rm requirements.txt
 
 echo "Done!"
