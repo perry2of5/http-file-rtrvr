feb272fdf3c8560066debcffc38a28f764dc0d84
Tim Perry <perry2of5@yahoo.com>
2024-12-04 11:56:20 -0800

    Minor updates

--- scripts/env-vars.zsh
+++ scripts/env-vars.zsh
@@ -3,28 +3,28 @@
 # we need the single quotes in the az command so they are wrapped in the double quotes so they'll be preserved
 export AIRFLOW_APP_NAME="'tim-dev-http-dwnld-asb'"
 
-export DPLMNT_SFFX="2024-12-02-v2"
+export DPLMNT_SFFX="2024-12-04-v1"
 
-export RESOURCE_GROUP="file-dwnld-job-resrc-grp-${DPLMNT_SFFX}"
-export ENVIRONMENT="fl-dwnld-jb-nvrnmnt-${DPLMNT_SFFX}"
+export RESOURCE_GROUP="http-rtrvr-rsrc-grp-${DPLMNT_SFFX}"
+export ENVIRONMENT="http-rtrvr-nvrnmnt-${DPLMNT_SFFX}"
 export LOCATION="westus"
-export JOB_NAME="file-download-job-${DPLMNT_SFFX}"
+export JOB_NAME="http-rtrvr-job-${DPLMNT_SFFX}"
 
-export CONTAINER_IMAGE_NAME="queue-reader-job:1.0"
+export CONTAINER_IMAGE_NAME="http-rtrvr-queue-reader-job:1.0"
 
 # need repeatable ACR name for now.
-export CONTAINER_REGISTRY_NAME="tdpacareg202411130843125399139870"
+# export CONTAINER_REGISTRY_NAME="tdpacareg202411130843125399139870"
 # this will make an ACR name based off the date and time with a random number appended to the end.
-# export CONTAINER_REGISTRY_NAME="tdpacareg$(date "+%Y%m%dT%H%M%Srnd")$(head -200 /dev/urandom | cksum | cut -f1 -d " ")"
+export CONTAINER_REGISTRY_NAME="tdpacareg$(date "+%Y%m%dT%H%M%Srnd")$(head -200 /dev/urandom | cksum | cut -f1 -d " ")"
 
-export IDENTITY_NAME="asb-http-retriever-identity-${DPLMNT_SFFX}"
+export IDENTITY_NAME="http-rtrvr-identity-asb-${DPLMNT_SFFX}"
 
 # ASB = Azure Service Bus
 export ASB_SUBSCRIPTION=$(az account show --query id --output tsv)
-export ASB_NAMESPACE=http-download-asb-${DPLMNT_SFFX}
+export ASB_NAMESPACE=http-rtrvr-asb-${DPLMNT_SFFX}
 export ASB_RESOURCE_GRP=${RESOURCE_GROUP}
-export ASB_DWNLD_REQ_QUEUE_NAME=file-download-request
-export ASB_DWNLD_CMPLT_TOPIC_NAME=file-download-complete
+export ASB_DWNLD_REQ_QUEUE_NAME=file-rtrvr-request
+export ASB_DWNLD_CMPLT_TOPIC_NAME=file-rtrvr-complete
 # need to be data owner on the bus to get the number of messages to scale....
 export ASB_IAM_ROLE="Azure Service Bus Data Owner"
 
@@ -33,4 +33,4 @@ export STORAGE_ACCOUNT_URL=https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net
 export STORAGE_CONTAINER_NAME=testcontainer
 
 # if the identity is already created, you can get the client id by running the following command:
-# export IDENTITY_CLIENT_ID=$(az identity show --name "asb-http-rtvr-identity" --resource-group "${RESOURCE_GROUP}" --query clientId --output tsv)
+# export IDENTITY_CLIENT_ID=$(az identity show --name "${IDENTITY_NAME}" --resource-group "${RESOURCE_GROUP}" --query clientId --output tsv)
